f5a40972792b293f47ab4b323fe5fff928717f16
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-04-25 14:30:24 +0600

    Changes

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -10,12 +10,7 @@ function Contacts() {
         Souvenir shopping is one of the true joys of travelling. It’s part of the fun of exploring a new place, and you get to bring a little piece of it home with you!
         </Header>
         </>
-        // <article>
-        //     <h1>This is contacts page</h1>
-        //     <p>
-        //     Souvenir shopping is one of the true joys of travelling. It’s part of the fun of exploring a new place, and you get to bring a little piece of it home with you!
-        //     </p>
-        // </article>
+        
     )
 }
 export default Contacts;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,12 +9,7 @@ function Home() {
         Souvenir shopping is one of the true joys of travelling. It’s part of the fun of exploring a new place, and you get to bring a little piece of it home with you!  
         </Header>
         </>
-        // <article>
-        //     <h1>This is home page</h1>
-        //     <p>
-        //     Souvenir shopping is one of the true joys of travelling. It’s part of the fun of exploring a new place, and you get to bring a little piece of it home with you!
-        //     </p>
-        // </article>
+       
     );
 }
 export default Home;
