a9d8927e225d4f070c7d1455f6e71d8eb15aa89c
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-04-29 13:48:57 +0600

    Added assets

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,9 @@
 import productImage from "../assets/honey.jpeg";
 import productImage2 from "../assets/dandelion.jpeg";
-import productImage3 from "../assets/orange juice7.jpeg";
+import productImage3 from "../assets/pine.jpeg";
+import productImage4 from "../assets/slippers.jpeg";
+import productImage5 from "../assets/wool bag.jpeg";
+import productImage6 from "../assets/cream.jpeg";
 
 const products = [
   {
@@ -19,31 +22,31 @@ const products = [
   },
   {
     image: productImage3,
-    productId: "seabuckthorn-jam",
-    title: "Seabuckthorn jam",
+    productId: "pine-cone-jam",
+    title: "Pine cone jam",
     description: `Honey is produced by bees collecting nectar and honeydew for use as sugars consumed to support metabolism of muscle activity during foraging or to be stored as a long-term food supply.[14][15] During foraging, bees use part of the nectar collected to support metabolic activity of flight muscles, with the majority of collected nectar destined for regurgitation, digestion, and storage as honey.[14][16] In cold weather or when other food sources are scarce, adult and larval bees use stored honey as food.`,
     price: 12,
   },
   {
-    image: productImage,
-    productId: "black-current-jam",
-    title: "Black current jam",
+    image: productImage4,
+    productId: "slippers",
+    title: "Slippers",
     description: `Honey is collected from wild bee colonies or from domesticated beehives. On average, a hive will produce about 29 kilograms (65 lb) of honey per year.[24] Wild bee nests are sometimes located by following a honeyguide bird.
 
     To safely collect honey from a hive, beekeepers typically pacify the bees using a bee smoker. The smoke triggers a feeding instinct (an attempt to save the resources of the hive from a possible fire), making them less aggressive, and obscures the pheromones the bees use to communicate. The honeycomb is removed from the hive and the honey may be extracted from it either by crushing or by using a honey extractor. The honey is then usually filtered to remove beeswax and other debris..`,
     price: 10,
   },
   {
-    image: productImage,
-    productId: "seabuckthorn-juice",
-    title: "Seabuckthorn juice",
+    image: productImage5,
+    productId: "wool-bag",
+    title: "Wool bag",
     description: `Honey is sometimes adulterated by the addition of other sugars, syrups, or compounds to change its flavor or viscosity, reduce cost, or increase the fructose content to stave off crystallization. Adulteration of honey has been practiced since ancient times, when honey was sometimes blended with plant syrups such as maple, birch, or sorghum and sold to customers as pure honey. Sometimes crystallized honey was mixed with flour or other fillers, hiding the adulteration from buyers until the honey was liquefied. In modern times the most common adulterant became clear, almost-flavorless corn syrup; the adulterated mixture can be very difficult to distinguish from pure honey.`,
     price: 8,
   },
   {
-    image: productImage,
-    productId: "seabuckthorn-jam",
-    title: "Seabuckthorn jam",
+    image: productImage6,
+    productId: "cream",
+    title: "Natural cream",
     description: `The physical properties of honey vary, depending on water content, the type of flora used to produce it (pasturage), temperature, and the proportion of the specific sugars it contains. Fresh honey is a supersaturated liquid, containing more sugar than the water can typically dissolve at ambient temperatures. At room temperature, honey is a supercooled liquid, in which the glucose precipitates into solid granules. This forms a semisolid solution of precipitated glucose crystals in a solution of fructose and other ingredients.`,
     price: 12,
   }
