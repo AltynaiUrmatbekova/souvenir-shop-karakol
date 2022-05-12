aec0b407bc22b365c5791414bfb6975076793015
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-05-12 11:19:05 +0600

    Changed

--- src/pages/contacts/Contacts.js
+++ src/pages/contacts/Contacts.js
@@ -5,7 +5,7 @@ import "./Contacts.module.css";
 function Contacts() {
   return (
     <>
-      <Header title="Order your presents" image={headerImage}>
+      <Header title="Order your presents!" image={headerImage}>
         Souvenir shopping is one of the true joys of travelling. It’s part of
         the fun of exploring a new place, and you get to bring a little piece of
         it home with you!
