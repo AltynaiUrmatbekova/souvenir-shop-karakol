2e108932733e47341ae21752bba0daec6b3bcb4f
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-21 11:14:28 +0600

    Added new products

--- src/data/categories.js
+++ src/data/categories.js
@@ -22,12 +22,9 @@ const categories = [
         description: "Notably, the Inazuma Souvenir Shop does not open until you max out the Sacred Sakura Tree. Nearly all of your Electro Sigils will go toward upgrading the tree and getting tons of very worthwhile rewards from it. After you fully upgrade it, you can head over to the Inazuma Souvenir Shop, but it really only has some weapon enhancement material and furniture blueprints — nothing super interesting.",
     },
 ];
-
 export function getCategories() {
     return categories;
   }
-  
-
   export function getCategory(categoryId) {
     return categories[
       categories.findIndex((category) => category.categoryId === categoryId)
