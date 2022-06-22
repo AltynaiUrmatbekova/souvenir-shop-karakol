df2f4c21306650b12403829e42e9614ffa2bc179
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-22 11:43:16 +0600

    create Homemade page

---
+++ src/pages/Homemade.js
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import Header from "../components/Header/Header";
+import { getHomemade } from "../data/homemades";
+
+function Homemade() {
+  const params = useParams();
+  const homemade = getHomemade(params.homemadeId);
+  console.log(getHomemade(params.homemadeId));
+
+  if (!homemade) {
+    return null;
+  }
+
+  return (
+    <>
+      <Header
+        title={homemade.title}
+        image={homemade.image}>
+        {homemade.description}
+      </Header>
+    </>
+  );
+}
+
+export default Homemade;
