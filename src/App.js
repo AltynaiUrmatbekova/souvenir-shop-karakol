0a8d60be786f57fbd4b075b537232b267f7ab06c
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-22 11:44:18 +0600

    added path in App to Homemade pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
-import Layout from "./components/Layout/Layout"
+import Layout from "./components/Layout/Layout";
 import Products from "./pages/Products";
 import { Route, Routes } from "react-router-dom";
-import Contacts from "./pages/contacts/Contacts"
+import Contacts from "./pages/contacts/Contacts";
 import Product from "./pages/Product";
 import NotFound from "./pages/NotFound";
 import Categories from "./pages/Categories";
 import Category from "./pages/Category";
+import Homemade from "./pages/Homemade";
+import Homemades from "./pages/Homemades";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<Product />} />
+          <Route path="/homemades" element={<Homemades />} />
+          <Route path="/homemades/:homemadeId" element={<Homemade />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/categories/:categoryId" element={<Category />} />
           <Route path="/contacts" element={<Contacts />} />
