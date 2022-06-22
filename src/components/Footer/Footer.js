2ed3c8a1a11d61e25d6e06976f1867301d328ea2
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-22 11:12:27 +0600

    deleted icons in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,28 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="Footer">
-      <ul>
-        <li>
-          <a href="/">
-          <i class="fa-brands fa-instagram"></i>
-          </a>
-        </li>
-        <li>
-          <a href="/">
-          <i class="fa-brands fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a href="/">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-        </li>
-        <li>
-          <a href="/">
-            <i className="fab fa-google-plus-g"></i>
-          </a>
-        </li>
-      </ul>
+      
       <div className="footer-logo">
         2022 All rights reserved 
       </div>
