0f743c640bd0ef688fae1acee783ee08bc6ef8b8
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-22 11:44:53 +0600

    added new NavItem in Nav to Homemades page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,33 +4,37 @@ import icon from "../../assets/shopping-cart-icon.jpg";
 import search from "../../assets/search.png";
 import telephone from "../../assets/phone.png";
 
-
 function Nav() {
-
   const styles = {
     width: "40px",
     height: "40px",
-    
-  }
+  };
   const styles1 = {
     width: "40px",
     height: "40px",
-  }
+  };
   const styles2 = {
     width: "20px",
     height: "20px",
-  }
-  
+  };
 
   return (
-        <ul className={classes.Nav}>
-          <NavItem url="/">Home</NavItem>
-          <NavItem url="/products"> Products</NavItem>
-          <NavItem url="/contacts">Contact</NavItem>
-          <NavItem url=""><img src={icon} alt="cart" style={styles}/></NavItem>
-          <NavItem url="#"><img src={search} alt="cart1" style={styles1}/></NavItem>
-          <NavItem url="#"><img src={telephone} alt="cart2" style={styles2}/>996705617968</NavItem>
-        </ul>
+    <ul className={classes.Nav}>
+      <NavItem url="/">Home</NavItem>
+      <NavItem url="/products">Products</NavItem>
+      <NavItem url="/homemades">Homemades</NavItem>
+      <NavItem url="/contacts">Contact</NavItem>
+      <NavItem url="">
+        <img src={icon} alt="cart" style={styles} />
+      </NavItem>
+      <NavItem url="#">
+        <img src={search} alt="cart1" style={styles1} />
+      </NavItem>
+      <NavItem url="#">
+        <img src={telephone} alt="cart2" style={styles2} />
+        996705617968
+      </NavItem>
+    </ul>
   );
 }
 export default Nav;
