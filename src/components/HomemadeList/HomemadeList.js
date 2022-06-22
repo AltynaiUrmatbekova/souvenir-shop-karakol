0535c8c20f9970b2e1b7c2533cf94c693a3de0d8
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-22 11:41:59 +0600

    create HomemadeList component

---
+++ src/components/HomemadeList/HomemadeList.js
@@ -0,0 +1,16 @@
+import HomemadeItem from "./HomemadeItem/HomemadeItem";
+import classes from "./HomemadeList.module.css";
+
+function HomemadeList({ homemades }) {
+  const homemadeItems = homemades.map(homemade => (
+    <HomemadeItem key={homemade.homemadeId} homemade={homemade} />
+  ));
+
+  return (
+    <div className={classes.HomemadeList}>
+      {homemadeItems}
+    </div>
+  );
+}
+
+export default HomemadeList;
