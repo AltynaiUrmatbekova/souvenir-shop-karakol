f7a03028c1cbd10a17814105bd49d4e7267f0675
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-23 13:27:16 +0600

    corrected localStorage in cartSlice and Appjs

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,5 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+function saveInStorage(items) {
+  localStorage.setItem('cartItems', JSON.stringify(items));
+}
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
@@ -14,6 +18,27 @@ const cartSlice = createSlice({
       else {
         store.items[action.payload] = 1;
       }
+      saveInStorage(store.items);
+    },
+    delete: (store, action) => {
+      delete store.items[action.payload];
+      saveInStorage(store.items);
+    },
+    increment: (store, action) => {
+      store.items[action.payload]++;
+      saveInStorage(store.items);
+    },
+    decrement: (store, action) => {
+      if (store.items[action.payload] > 1) {
+        store.items[action.payload]--;
+      }
+      else {
+        delete store.items[action.payload];
+      }
+      saveInStorage(store.items);
+    },
+    restore: (store, action) => {
+      store.items = JSON.parse(localStorage.getItem('cartItems') ?? '{}');
     }
   }
 });
