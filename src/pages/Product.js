14843e6ae62b8a80ec0ebb29ffc1a2d0e1215932
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-24 13:16:50 +0600

    Fotmat style css

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -12,18 +12,35 @@ function Product() {
   console.log(product.description);
   return (
     <>
-    <div style={{display: "flex", padding: "1rem 3rem"}}>
-        <div style={{padding: "1rem", textAlign: "center", lineHeight: "50px"}}>
-          <h2 style={{color: "black"}}>{product.title}</h2>
-          <p style={{color: "black"}}>{product.description}</p>
-          <button style={{width: "120px", height: "30px", backgroundColor: "white", color: "black", fontSize: "1rem", fontWeight: "300"}}>
-           {product.price}$
+      <div style={{ display: "flex", padding: "1rem 3rem" }}>
+        <div
+          style={{ padding: "1rem", textAlign: "center", lineHeight: "50px" }}
+        >
+          <h2 style={{ color: "black" }}>{product.title}</h2>
+          <p style={{ color: "black" }}>{product.description}</p>
+          <button
+            style={{
+              width: "120px",
+              height: "30px",
+              backgroundColor: "white",
+              color: "black",
+              fontSize: "1rem",
+              fontWeight: "300",
+            }}
+          >
+            {product.price}$
           </button>
           <CartButton productId={params.productId} />
         </div>
-        {<img style={{width: "400px"}} src={product.image} alt={product.title} />}
+        {
+          <img
+            style={{ width: "400px" }}
+            src={product.image}
+            alt={product.title}
+          />
+        }
       </div>
-      <CartButton productId={params.productId}/>
+      <CartButton productId={params.productId} />
     </>
   );
 }
