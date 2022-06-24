ba619c73ce89329ff4e7146c25c16b710a3fa30a
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-24 13:28:23 +0600

    Added some photos in Category

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -2,15 +2,11 @@ import CategoryItem from "./CategoryItem/CategoryItem";
 import classes from "./CategoryList.module.css";
 
 function CategoryList({ categories }) {
-  const categoryItems = categories.map(category => (
+  const categoryItems = categories.map((category) => (
     <CategoryItem key={category.categoryId} category={category} />
   ));
 
-  return (
-    <div className={classes.CategoryList}>
-      {categoryItems}
-    </div>
-  );
+  return <div className={classes.CategoryList}>{categoryItems}</div>;
 }
 
 export default CategoryList;
