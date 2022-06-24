8ee3b6d5797537c960a0afdc6a6eed2e61ddd140
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-24 13:25:32 +0600

    Formatted Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="Footer">
-      
-      <div className="footer-logo">
-        2022 All rights reserved 
-      </div>
+      <div className="footer-logo">2022 All rights reserved</div>
     </footer>
   );
 }
