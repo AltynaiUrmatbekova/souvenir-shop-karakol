e97b7f6ea14f2cbe892225f3b7d849895f5a539f
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-24 13:53:45 +0600

    Formated product.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,24 +1,24 @@
 import Header from "../components/Header/Header";
 import headerImage from "../assets/lentas.png";
-import ProductList from "../components/ProductList/ProductList"
+import ProductList from "../components/ProductList/ProductList";
 import { getProducts } from "../data/products";
 import Main from "../components/Main/Main";
 
 function Products() {
-    return (
-        <>
-        <Header 
-        title="Choose the best quality!"
-        image={headerImage}>
-            Souvenir shopping is one of the true joys of travelling. It’s part of the fun of exploring a new place, and you get to bring a little piece of it home with you!
-        </Header>
-        {/* {<main>
+  return (
+    <>
+      <Header title="Choose the best quality!" image={headerImage}>
+        Souvenir shopping is one of the true joys of travelling. It’s part of
+        the fun of exploring a new place, and you get to bring a little piece of
+        it home with you!
+      </Header>
+      {/* {<main>
             <h2>We provide natural, felt and wool products!</h2>
         </main> */}
-        <Main>
+      <Main>
         <ProductList products={getProducts()} />
-        </Main>
-        </>
-    );
+      </Main>
+    </>
+  );
 }
 export default Products;
