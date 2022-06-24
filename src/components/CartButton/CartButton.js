d5f740f9585fe697a4ae33b8734c13bf11109561
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-24 13:29:16 +0600

    Added button into CartButton.js

--- src/components/CartButton/CartButton.js
+++ src/components/CartButton/CartButton.js
@@ -4,10 +4,12 @@ export default function CartButton({ productId }) {
   const dispatch = useDispatch();
 
   function onAddToCart() {
-    dispatch({ type: 'cart/add', payload: productId });
+    dispatch({ type: "cart/add", payload: productId });
   }
 
   return (
-    <button className="CartButton" onClick={onAddToCart}>Add to card</button>
+    <button className="CartButton" onClick={onAddToCart}>
+      Add to card
+    </button>
   );
 }
