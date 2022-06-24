6f029a5c2bcd7b80390d79d02fed225ef7b77121
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-24 11:44:23 +0600

    Filter categories page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import CartButton from "../components/CartButton/CartButton";
-import Header from "../components/Header/Header";
 import { getProduct } from "../data/products";
 
 function Product() {
@@ -10,13 +9,20 @@ function Product() {
   if (!product) {
     return null;
   }
+  console.log(product.description);
   return (
     <>
-      <Header 
-      title={product.title} 
-      image={product.image}>
-      {product.description}
-      </Header>
+    <div style={{display: "flex", padding: "1rem 3rem"}}>
+        <div style={{padding: "1rem", textAlign: "center", lineHeight: "50px"}}>
+          <h2 style={{color: "black"}}>{product.title}</h2>
+          <p style={{color: "black"}}>{product.description}</p>
+          <button style={{width: "120px", height: "30px", backgroundColor: "white", color: "black", fontSize: "1rem", fontWeight: "300"}}>
+           {product.price}$
+          </button>
+          <CartButton productId={params.productId} />
+        </div>
+        <img style={{width: "400px"}} src={product.image} alt={product.title} />
+      </div>
       <CartButton productId={params.productId}/>
     </>
   );
