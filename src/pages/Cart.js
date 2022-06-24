3e2bc513ed749ccc618e3c416a7f3679f58bd3bb
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-24 13:18:03 +0600

    Added style in cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,23 +5,41 @@ import { getProducts } from "../data/products";
 import { Link } from "react-router-dom";
 
 function Cart() {
-  const items = useSelector(store => store.cart.items);
+  const items = useSelector((store) => store.cart.items);
   const dispatch = useDispatch();
   const products = getProducts();
 
   let total = 0;
   let output = products
-    .filter(product => items[product.productId])
-    .map(product => {
+    .filter((product) => items[product.productId])
+    .map((product) => {
       total += product.price * items[product.productId];
 
       return (
         <div>
-          <Link to="">{product.title}</Link> {items[product.productId]} ${product.price * items[product.productId]}
-
-          <button onClick={() => dispatch({ type: "cart/decrement", payload: product.productId })}>-</button>
-          <button onClick={() => dispatch({ type: "cart/increment", payload: product.productId })}>+</button>
-          <button onClick={() => dispatch({ type: "cart/delete", payload: product.productId })}>Delete</button>
+          <Link to="">{product.title}</Link> {items[product.productId]} $
+          {product.price * items[product.productId]}
+          <button
+            onClick={() =>
+              dispatch({ type: "cart/decrement", payload: product.productId })
+            }
+          >
+            -
+          </button>
+          <button
+            onClick={() =>
+              dispatch({ type: "cart/increment", payload: product.productId })
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={() =>
+              dispatch({ type: "cart/delete", payload: product.productId })
+            }
+          >
+            Delete
+          </button>
         </div>
       );
     });
@@ -30,19 +48,15 @@ function Cart() {
     output = "No items in the cart.";
   }
 
-
   return (
     <>
-      <Header
-        title="Your Shopping Cart"
-        image={image}>
+      <Header title="Your Shopping Cart" image={image}>
         Please review items in your cart.
       </Header>
       <div>
         {output}
         <hr />
         Total: ${total}
-
         <Link to="/checkout">Checkout</Link>
       </div>
     </>
