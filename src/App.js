baca8615d3e7564883529108d7d89723a85666c9
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-28 15:52:01 +0600

    Added Checkout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import Home from "./pages/Home";
-import Layout from "./components/Layout/Layout";
+import Contacts from "./pages/Contacts";
 import Products from "./pages/Products";
 import { Route, Routes } from "react-router-dom";
-import Contacts from "./pages/contacts/Contacts";
+import Layout from "./components/Layout/Layout";
 import Product from "./pages/Product";
 import NotFound from "./pages/NotFound";
 import Categories from "./pages/Categories";
 import Category from "./pages/Category";
-import Homemade from "./pages/Homemade";
-import Homemades from "./pages/Homemades";
 import Cart from "./pages/Cart";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 import Checkout from "./pages/Checkout";
+import Auth from "./pages/Auth";
+import SignOut from "./pages/SignOut";
 
 function App() {
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch({ type: 'cart/restore' });
-  }, [dispatch]);
-
   return (
     <div className="App">
       <Layout>
@@ -28,13 +20,13 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:productId" element={<Product />} />
-          <Route path="/homemades" element={<Homemades />} />
-          <Route path="/homemades/:homemadeId" element={<Homemade />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/categories/:categoryId" element={<Category />} />
           <Route path="/contacts" element={<Contacts />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/signout" element={<SignOut />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
