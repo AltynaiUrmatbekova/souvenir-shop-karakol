3198d5ccc3dd3ee93cbd51e717978e784199de00
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-29 12:47:54 +0600

    Changed CartDisplay

--- src/components/CartDisplay/CartDisplay.js
+++ src/components/CartDisplay/CartDisplay.js
@@ -6,57 +6,71 @@ import classes from "./CartDisplay.module.css";
 import { increment, decrement, remove } from "../../redux/cartSlice";
 
 function CartDisplay() {
-  const items = useSelector(store => store.cart.items);
+  const items = useSelector((store) => store.cart.items);
   const dispatch = useDispatch();
   const products = getProducts();
 
   let total = 0;
   let output = products
-    .filter(product => items[product.productId])
-    .map(product => {
+    .filter((product) => items[product.productId])
+    .map((product) => {
       total += product.price * items[product.productId];
 
       return (
         <div className={classes.item} key={product.productId}>
-          <Link to={"/cart/" + product.productId} className={classes.productCard}>
-            <img src={product.image} alt={product.title} className={classes.productImage} />
+          <Link
+            to={"/cart/" + product.productId}
+            className={classes.productCard}
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className={classes.productImage}
+            />
             {product.title}
-          </Link> 
+          </Link>
           <div>
-            <button 
-            className={classes.changeButton} 
-            onClick={() => dispatch(decrement(product.productId))}>-</button>
+            <button
+              className={classes.changeButton}
+              onClick={() => dispatch(decrement(product.productId))}
+            >
+              -
+            </button>
             <span className={classes.productQuantity}>
               {items[product.productId]}
-            </span> 
-            <button 
-              className={classes.changeButton} 
-              onClick={() => dispatch(increment(product.productId))}>+</button>
+            </span>
+            <button
+              className={classes.changeButton}
+              onClick={() => dispatch(increment(product.productId))}
+            >
+              +
+            </button>
           </div>
           <span className={classes.productPrice}>
             ${product.price * items[product.productId]}
-          </span> 
-          <button 
-            className={classes.deleteButton} 
-            onClick={() => dispatch(remove(product.productId))}>Delete</button>
+          </span>
+          <button
+            className={classes.deleteButton}
+            onClick={() => dispatch(remove(product.productId))}
+          >
+            Delete
+          </button>
         </div>
       );
     });
 
-  let checkoutPath = '/checkout'; 
+  let checkoutPath = "/checkout";
 
   if (!output.length) {
     output = (
       <>
         <div>No items in the cart.</div>
         <Link to="/shop">
-          <button className={classes.checkoutButton}>
-            Continue shopping
-          </button>
+          <button className={classes.checkoutButton}>Continue shopping</button>
         </Link>
       </>
     );
-    checkoutPath = '/home';
+    checkoutPath = "/home";
   }
 
   return (
