4ac204bd247becc9008c0d642b22665d03f30c7d
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2022-06-29 12:49:54 +0600

    Fixed CartLink.js

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -1,10 +1,13 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import cart from "../../assets/shopping-cart-icon.jpg"
+import cart from "../../assets/shopping-cart-icon.jpg";
 
 export default function CartLink() {
-  const number = useSelector(store => {
-    return Object.values(store.cart.items).reduce((sum, number) => sum + number, 0);
+  const number = useSelector((store) => {
+    return Object.values(store.cart.items).reduce(
+      (sum, number) => sum + number,
+      0
+    );
   });
 
   const styles = {
@@ -12,6 +15,9 @@ export default function CartLink() {
     height: "40px",
   };
   return (
-    <NavLink to="/cart"><img src={cart} alt="cart" style={styles}/>{number}</NavLink>
+    <NavLink to="/cart">
+      <img src={cart} alt="cart" style={styles} />
+      {number}
+    </NavLink>
   );
 }
